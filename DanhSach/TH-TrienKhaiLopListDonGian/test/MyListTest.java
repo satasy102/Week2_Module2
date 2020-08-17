6a51f82237ec4045c62371cc2bfa18188c99ace6
Do Chanh Tin <@gmail>
2020-08-17 16:05:34 +0700

    Update

---
+++ DanhSach/TH-TrienKhaiLopListDonGian/test/MyListTest.java
@@ -0,0 +1,24 @@
+
+
+class MyListTest  {
+        public static void main(String[] args) {
+                MyList<Integer> listInteger = new MyList<Integer>();
+                listInteger.add(1);
+                listInteger.add(2);
+                listInteger.add(3);
+                listInteger.add(4);
+                listInteger.add(5);
+
+
+                System.out.println("element 0: "+listInteger.get(0));
+                System.out.println("element 1: "+listInteger.get(1));
+                System.out.println("element 2: "+listInteger.get(2));
+                System.out.println("element 3: "+listInteger.get(3));
+                System.out.println("element 4: "+listInteger.get(4));
+
+
+                listInteger.get(-1);
+                System.out.println("element -1: " + listInteger.get(-1));
+        }
+
+}
